284f8a125643d59b2494540be136c6b85ac9ec33
s-woegerbauer <simon.woeger@gmail.com>
2023-10-19 17:41:25 +0200

    Added Source Code
    - Added main logic
    - Added folder structure

---
+++ CCC_Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCC_2023_Setup
+{
+    public static class CCC_Helper
+    {
+        private static string GetOutputFilePath(string filePath)
+        {
+            return $"{Directory.GetCurrentDirectory()}\\output\\{filePath.Split('\\')[^1]}";
+        }
+
+        public static void RunAll()
+        {
+            RunTest();
+
+            foreach (string filePath in Directory.GetFiles($"{Directory.GetCurrentDirectory()}\\input"))
+            {
+                string[] result = Program.Solution(File.ReadAllLines(filePath));
+                File.WriteAllLines(GetOutputFilePath(filePath), result);
+            }
+        }
+
+        private static void RunTest()
+        {
+            string testFilePath = $"{Directory.GetCurrentDirectory()}\\test.txt";
+            string[] result = Program.Solution(File.ReadAllLines(testFilePath));
+
+            foreach (string line in result)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CCC_2023_Setup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CCC_Helper.RunAll();
+        }
+
+        public static string[] Solution(string[] input)
+        {
+            List<string> output = new List<string>();
+
+            output.Add(input[0]);
+
+            return output.ToArray();
+        }
+    }
+}
